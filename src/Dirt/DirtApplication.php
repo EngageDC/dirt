99c8f2714d823f5493728b01988c082aebfbeb0f
Mathias Hansen <me@codemonkey.io>
2015-03-26 16:40:56 -0400

    Bumped version number and updated default options

--- src/Dirt/DirtApplication.php
+++ src/Dirt/DirtApplication.php
@@ -18,7 +18,7 @@ class DirtApplication extends Application {
 ';
 
     private static $name = 'dirt [Done In Record Time]';
-    private static $version = '1.1.4';
+    private static $version = '1.2';
 
     public function __construct() {
 
@@ -33,10 +33,10 @@ class DirtApplication extends Application {
             $container->get('Dirt\Command\CreateCommand'),
             $container->get('Dirt\Command\DeployCommand'),
             $container->get('Dirt\Command\ApplyCommand'),
+            $container->get('Dirt\Command\UpdateCommand'),
             $container->get('Dirt\Command\TransferCommand'),
             $container->get('Dirt\Command\TransferDatabaseCommand'),
-            $container->get('Dirt\Command\TransferUploadsCommand'),
-            $container->get('Dirt\Command\UpdateCommand')
+            $container->get('Dirt\Command\TransferUploadsCommand')
         ));
     }
 
@@ -52,7 +52,7 @@ class DirtApplication extends Application {
 
             new InputOption('--help',           '-h', InputOption::VALUE_NONE, 'Display this help message.'),
             new InputOption('--quiet',          '-q', InputOption::VALUE_NONE, 'Do not output any message.'),
-            new InputOption('--verbose',        '-v', InputOption::VALUE_NONE, 'Increase verbosity of messages.'),
+            new InputOption('--verbose',        '-v|vv|vvv', InputOption::VALUE_NONE, 'Increase the verbosity of messages: 1 for normal output, 2 for more verbose output and 3 for debug'),
             new InputOption('--version',        '-V', InputOption::VALUE_NONE, 'Display this application version.'),
         ));
     }
